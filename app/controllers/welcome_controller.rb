cdb6d42667c544c3cf23eef24dcaee69396f95f0
Johnty <johntysimon80@gmail.com>
2019-09-29 11:46:19 +0530

    create basic routes and welcome pages

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,23 @@
+class WelcomeController < ApplicationController
+
+  def home
+
+  end
+
+  def about
+
+  end
+
+  def studentportal
+
+  end
+
+  def facultyportal
+
+  end
+
+  def adminportal
+
+  end
+
+end
