ef691c5b485a77e0a1fe0bee2e14a42d93cf3a32
Johnty <johntysimon90@gmail.com>
2019-11-05 11:13:28 +0530

    Update the permissions for students

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
 
-  def studentportal
+  before_action :require_student
+  before_action :set_student, only: [:show]
+  before_action :require_same_student
+
+
+  def show
     redirect_to root_path if !logged_in?
   end
 
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def require_same_student
+    if current_student.enrollnumber != @student.enrollnumber
+      flash[:danger] = "You can only edit or update your profile"
+      redirect_to root_path
+    end
+  end
 
 end
